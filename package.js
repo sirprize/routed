b668dcd17a8a7484ad520bbac55023fb54a08443
sirprize <hoegl@backdoor.ch>
2012-08-10 07:20:50 +0200

    Move packaged files into root dir

---
+++ package.js
@@ -0,0 +1,27 @@
+var profile = (function(){
+
+    var miniExcludes = {
+            "LICENSE": 1,
+            "spirr/package": 1,
+            "spirr/package.json": 1,
+            "readme.md": 1
+        },
+        isTestRe = /\/tests\//
+    ;
+
+    return {
+        resourceTags: {
+            test: function(filename, moduleId) {
+                return isTestRe.test(filename);
+            },
+            
+            miniExclude: function(filename, moduleId) {
+                return isTestRe.test(filename) || moduleId in miniExcludes || /\/vendor\//.test(moduleId);
+            },
+
+            amd: function(filename, moduleId) {
+                return /\.js$/.test(filename);
+            }
+        }
+    };
+})();
